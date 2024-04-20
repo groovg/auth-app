cfbeec88a9907ecbbb1b0d095f90baa66cb1dc70
Hlib Dmytriiev <action.dmitrieff@gmail.com>
2024-04-20 18:28:36 +0300

    added GitHub and Google auth with account linking

--- components/auth/login-form.tsx
+++ components/auth/login-form.tsx
@@ -7,6 +7,7 @@ import { CardWrapper } from "@/components/auth/card-wrapper";
 import { useState, useTransition } from "react";
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
+import { useSearchParams } from "next/navigation";
 import { LoginSchema } from "@/schemas";
 import { Input } from "@/components/ui/input";
 
@@ -24,8 +25,18 @@ import { FormSuccess } from "@/components/form-success";
 import { login } from "@/actions/login";
 
 export const LoginForm = () => {
-  const [error, setError] = useState<string | undefined>("");
-  const [success, setSuccess] = useState<string | undefined>("");
+  const searchParams = useSearchParams();
+  const urlError =
+    searchParams.get("error") === "OAuthAccountNotLinked"
+      ? "Email already in use with different provider"
+      : "";
+
+  const [error, setError] = useState<string | undefined>(
+    ""
+  );
+  const [success, setSuccess] = useState<
+    string | undefined
+  >("");
   const [isPending, startTransition] = useTransition();
 
   const form = useForm<z.infer<typeof LoginSchema>>({
@@ -36,14 +47,17 @@ export const LoginForm = () => {
     },
   });
 
-  const onSubmit = (values: z.infer<typeof LoginSchema>) => {
+  const onSubmit = (
+    values: z.infer<typeof LoginSchema>
+  ) => {
     setError("");
     setSuccess("");
 
     startTransition(() => {
       login(values).then((data) => {
-        setError(data.error);
-        setSuccess(data.success);
+        setError(data?.error);
+        // TODO: Add when we add 2FA
+        // setSuccess(data.success);
       });
     });
   };
@@ -55,7 +69,9 @@ export const LoginForm = () => {
       backButtonHref="/auth/register"
       showSocial>
       <Form {...form}>
-        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
+        <form
+          onSubmit={form.handleSubmit(onSubmit)}
+          className="space-y-6">
           <div className="space-y-4">
             <FormField
               control={form.control}
@@ -94,9 +110,12 @@ export const LoginForm = () => {
               )}
             />
           </div>
-          <FormError message={error} />
+          <FormError message={error || urlError} />
           <FormSuccess message={success} />
-          <Button disabled={isPending} type="submit" className="w-full">
+          <Button
+            disabled={isPending}
+            type="submit"
+            className="w-full">
             Login
           </Button>
         </form>
