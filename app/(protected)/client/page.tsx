6caa6f1ea403028c5d6d17931aa94bf33757e997
Hlib Dmytriiev <action.dmitrieff@gmail.com>
2024-04-22 01:11:02 +0300

    added user and client components in panel

---
+++ app/(protected)/client/page.tsx
@@ -0,0 +1,15 @@
+"use client";
+
+import { UserInfo } from "@/components/user-info";
+import { useCurrentUser } from "@/hooks/use-current-user";
+import { currentUser } from "@/lib/auth";
+
+const ClientPage = () => {
+  const user = useCurrentUser();
+
+  return (
+    <UserInfo label="📱Client component" user={user} />
+  );
+};
+
+export default ClientPage;
--- auth.ts
+++ auth.ts
@@ -58,6 +58,11 @@ export const {
       if (token.role && session.user) {
         session.user.role = token.role as UserRole;
       }
+
+      if (session.user) {
+        session.user.isTwoFactorEnabled =
+          token.isTwoFactorEnabled as boolean;
+      }
       return session;
     },
     async jwt({ token }) {
@@ -68,6 +73,8 @@ export const {
       if (!existingUser) return token;
 
       token.role = existingUser.role;
+      token.isTwoFactorEnabled =
+        existingUser.isTwoFactorEnabled;
 
       return token;
     },
